c2999a5fc4afb9872f18cd9fbcd3d1d2fdbe3d84
suzusuke <suzusuke0806@gmail.com>
2022-12-24 21:52:30 +0900

    dependency pack changed to gin

--- main.go
+++ main.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-type Todo struct {
-	Title string
-	Done  bool
-}
-
-type TodoPageData struct {
-	PageTitle string
-	Todos     []Todo
-}
-
 func main() {
-	tmpl := template.Must(template.ParseFiles("layout.html"))
-	http.HandleFunc("/",
-		func(
-			w http.ResponseWriter,
-			r *http.Request,
-		) {
-			data := TodoPageData{
-				PageTitle: "My TODO list",
-				Todos: []Todo{
-					{Title: "Task 1", Done: false},
-					{Title: "Task 2", Done: true},
-					{Title: "Task 3", Done: true},
-				},
-			}
-			tmpl.Execute(w, data)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
 		})
-	http.ListenAndServe("localhost:8080", nil)
+	})
+	r.Run()
 }
