a33d682a8c5f3d7d02066cba662d7f51df534abd
suzusuke <suzusuke0806@gmail.com>
2022-12-25 19:47:05 +0900

    added query param URL

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -15,7 +14,6 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-// albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
@@ -38,6 +36,13 @@ func main() {
 	r.GET("/:name/:id", GetPerson)
 
 	r.GET("/moreJSON", GetMsg)
+
+	r.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname") // c.Request.URL.Query().Get("lastname") へのショートカット
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
 
 	s := &http.Server{
 		Addr:           "localhost:8080",
@@ -95,12 +100,10 @@ func GetGopherPhoto(c *gin.Context) {
 	c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
 }
 
-// GetAlbums responds with the list of all albums as JSON.
 func GetAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-// postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
@@ -115,8 +118,6 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
 
